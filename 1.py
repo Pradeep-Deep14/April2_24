3d328f7717e38251591358175dd9ccb6769eda5d
Pradeep-Deep14 <123350286+Pradeep-Deep14@users.noreply.github.com>
2024-04-02 09:59:57 +0530

    first commit

---
+++ 1.py
@@ -0,0 +1,13 @@
+Dict,sum_={},0
+Dict[(1,2,4)]=8
+Dict[(4,2,1)]=10
+Dict[(1,2)]=12
+for k in Dict:
+    sum_+=Dict[k]
+print(len(Dict)+sum_)
+
+
+#for loop runs and places the values in set
+#sum = 8+10+12=30
+#print(len(Dict)+sum_)= 30 + 3 = 33
+#33
